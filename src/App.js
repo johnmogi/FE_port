c88c1029b8b76ac67b7f538fcf58b016fca4bf46
johnmogi <anguru@gmail.com>
2023-12-01 15:56:50 +0000

    2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,85 @@
-// ... other imports
-import portfolioData from "./data";
+import React, { useState } from "react";
+import portfolioData from "./data"; // Import your project data
+import {
+  Card,
+  Button,
+  Modal,
+  Container,
+  Row,
+  Col,
+  Navbar,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [currentProject, setCurrentProject] = useState({});
+
+  const handleShow = (project) => {
+    setCurrentProject(project);
+    setShowModal(true);
+  };
+
+  const handleClose = () => setShowModal(false);
+
+  const getExcerpt = (text) => {
+    return text.length > 100 ? text.substring(0, 100) + "..." : text;
+  };
+
   return (
-    <div className="container">
-      <h1>My Portfolio</h1>
-      {portfolioData.map((project) => (
-        <div key={project.id} className="card mb-3">
-          <div className="card-body">
-            <h5 className="card-title">{project.title}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">{project.year}</h6>
-            <p className="card-text">{project.description}</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">
+          <img
+            src="/path/to/your/logo.png" // Replace with your logo path
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="Logo"
+          />
+          {" My Portfolio"}
+        </Navbar.Brand>
+      </Navbar>
+
+      <Container className="mt-4">
+        <h1>My Portfolio</h1>
+        <Row>
+          {portfolioData.map((project) => (
+            <Col md={4} key={project.id} className="mb-4">
+              <Card>
+                <Card.Img
+                  variant="top"
+                  src={project.thumbnail || "https://via.placeholder.com/150"}
+                />
+                <Card.Body>
+                  <Card.Title>{project.title}</Card.Title>
+                  <Card.Text>{getExcerpt(project.description)}</Card.Text>
+                  <Button variant="primary" onClick={() => handleShow(project)}>
+                    Learn More
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+
+        {/* Modal */}
+        <Modal show={showModal} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>{currentProject.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>{currentProject.description}</p>
+            {/* You can add more content here such as images or links */}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Container>
+    </>
   );
 }
 
--- src/data.js
+++ src/data.js
@@ -5,6 +5,9 @@ const portfolioData = [
     year: "2022 - Present",
     description:
       "As the IT Manager, I spearheaded the maintenance and redesign of three major websites: solio.co.il, hairmax.co.il, and silkn.co.il. My role involved developing custom functionalities, enhancing site performance, and managing the office network.",
+    excerpt:
+      "Led the redesign and maintenance of three major websites, enhancing performance and functionality.",
+    thumbnail: "https://via.placeholder.com/150", // Placeholder image
   },
   {
     id: 2,
@@ -12,6 +15,9 @@ const portfolioData = [
     year: "2022",
     description:
       "Successfully completed a comprehensive 500-hour DevOps bootcamp. The program covered advanced GitHub techniques, CI/CD pipelines, and cloud deployment on Azure and AWS, equipping me with key skills for modern development workflows.",
+    excerpt:
+      "Completed a 500-hour DevOps bootcamp, mastering advanced GitHub, CI/CD, and cloud deployment.",
+    thumbnail: "https://via.placeholder.com/150", // Placeholder image
   },
   {
     id: 3,
@@ -19,6 +25,9 @@ const portfolioData = [
     year: "2021",
     description:
       "Collaborated with Renovatio on project management and site development. My focus was on WordPress site development using Elementor and extensive use of jQuery, delivering high-quality web solutions as a freelance developer.",
+    excerpt:
+      "Managed and developed WordPress sites, specializing in Elementor and jQuery as a freelance developer.",
+    thumbnail: "https://via.placeholder.com/150", // Placeholder image
   },
   {
     id: 4,
@@ -26,6 +35,9 @@ const portfolioData = [
     year: "2020",
     description:
       "Completed an intensive full-stack development course totaling 800 hours. Gained proficiency in JavaScript, React, Angular, MySQL, Node.js, and PHP OOP, including Redux. The course included hands-on projects and tests, building a strong foundation in web development.",
+    excerpt:
+      "Gained full-stack development expertise in an 800-hour course, covering JS, React, Angular, and more.",
+    thumbnail: "https://via.placeholder.com/150", // Placeholder image
   },
   {
     id: 5,
@@ -33,6 +45,9 @@ const portfolioData = [
     year: "2020 - 2021",
     description:
       "Managed client projects, focusing on responsive design and infrastructure upgrades. My role involved ensuring timely delivery and high-quality output for various web development projects.",
+    excerpt:
+      "Oversaw client projects, focusing on responsive design and timely delivery at Upsite & Devinesites.",
+    thumbnail: "https://via.placeholder.com/150", // Placeholder image
   },
   {
     id: 6,
@@ -40,6 +55,9 @@ const portfolioData = [
     year: "2017 - 2020",
     description:
       "As an in-house developer for an internal marketing company, I supported over 200 campaigns, specializing in WordPress development. My contributions included optimizing server solutions, significantly reducing monthly costs.",
+    excerpt:
+      "Supported 200+ campaigns as a WordPress developer, optimizing server solutions and reducing costs.",
+    thumbnail: "https://via.placeholder.com/150", // Placeholder image
   },
 ];
 
